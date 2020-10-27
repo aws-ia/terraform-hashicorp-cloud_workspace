183b00e8dfe5cd470be6a763834ab0dac4f9d1df
Tony Vattathil <avattathil@gmail.com>
2020-10-27 10:15:31 -0700

    start by overwriting backend.hcl

--- main.tf
+++ main.tf
@@ -8,23 +8,23 @@ terraform {
   required_version = ">= 0.13"
 }
 locals {
-    delimiter = "-"
-    org_prefix = "aws-quickstart"
-    random_workspace = "${local.org_prefix}${local.delimiter}${random_string.rand4.result}"
-    random_org = "${local.org_prefix}${local.delimiter}${random_pet.name.id}"
+  delimiter        = "-"
+  org_prefix       = "aws-quickstart"
+  random_workspace = "${local.org_prefix}${local.delimiter}${random_string.rand4.result}"
+  random_org       = "${local.org_prefix}${local.delimiter}${random_pet.name.id}"
 }
 resource "random_pet" "name" {
   length = 1
 }
 
 resource "random_string" "rand4" {
-  length = 4
+  length  = 4
   special = false
-  upper = false 
+  upper   = false
 }
 
 resource "tfe_organization" "qs-org" {
-  name         = var.tfe_organization == "" ? local.random_org : var.tfe_organization
+  name  = var.tfe_organization == "" ? local.random_org : var.tfe_organization
   email = "admin@your-company.com"
 }
 
@@ -33,15 +33,15 @@ resource "tfe_workspace" "qs-workspace" {
   organization = tfe_organization.qs-org.name
 }
 
-resource  "null_resource" "backend_file" {
+resource "null_resource" "backend_file" {
   depends_on = [tfe_workspace.qs-workspace]
   provisioner "local-exec" {
-  command =  "echo  workspaces '{' name = \\\"${tfe_workspace.qs-workspace.name}\\\" '}' >> backend.hcl"
+    command = "echo  workspaces '{' name = \\\"${tfe_workspace.qs-workspace.name}\\\" '}' > backend.hcl"
   }
   provisioner "local-exec" {
-  command =  "echo hostname = \\\"app.terraform.io\\\" >> backend.hcl"
+    command = "echo hostname = \\\"app.terraform.io\\\" >> backend.hcl"
   }
   provisioner "local-exec" {
-  command =  "echo  organization = \\\"${tfe_organization.qs-org.name}\\\" >> backend.hcl"
+    command = "echo  organization = \\\"${tfe_organization.qs-org.name}\\\" >> backend.hcl"
   }
 }
